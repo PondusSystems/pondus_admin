295b0a34d21c87625ef65995399d5768f7fa4709
Shoaib Farooka <shoaibfarooka@gmail.com>
2024-10-19 23:59:49 +0500

    sportx added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const UserRoutes = require('./userRoutes');
-const tenantRoutes = require('./tenantRoutes');
+const TenantRoutes = require('./tenantRoutes');
 
 const router = express.Router();
 
 // Set up routes
 router.use('/user', UserRoutes);
-router.use('/tenant', tenantRoutes);
+router.use('/tenant', TenantRoutes);
 
 module.exports = router;
